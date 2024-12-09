74a41d897199158911f6d81d2b89c74e17c62997
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-12-09 23:42:18 +0530

    main file backend

---
+++ fullstackbasic/backend/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+
+const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("Server is ready");
+// });
+
+app.get("/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: "a joke",
+      content: "This is a joke",
+    },
+    {
+      id: 2,
+      title: "Another joke",
+      content: "This is another joke",
+    },
+    {
+      id: 3,
+      title: "third joke",
+      content: "This is third joke",
+    },
+    {
+      id: 4,
+      title: "forth joke",
+      content: "This is forth joke",
+    },
+    {
+      id: 5,
+      title: "fifth joke",
+      content: "This is fifth joke",
+    },
+  ];
+  res.send(jokes);
+});
+
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
+  console.log(`Serve at https://localhost:${port}`);
+});
