549c18bd20602cf710b3ed93e7f8a4bbb1319cdb
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-12-18 23:45:01 +0530

    product model

--- Part 5 - Data modelling for backend with mongoose/models/ecommerce/product.models.js
+++ Part 5 - Data modelling for backend with mongoose/models/ecommerce/product.models.js
@@ -1,17 +1,37 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema(
+  {
     description: {
-        required: true,
-        type: String
+      required: true,
+      type: String,
     },
     name: {
-        required: true,
-        type: String
+      required: true,
+      type: String,
     },
     productImage: {
-        type: String
-    }
-}, {timestamps: true})
+      type: String,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-export const Product = mongoose.model("Product", productSchema)
+export const Product = mongoose.model("Product", productSchema);
